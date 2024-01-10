02dfca2ba8dd7375d48a464dc4b41146c2f4d067
Kasten33 <hurlrykaden@gmail.com>
2024-01-09 18:03:27 -0700

    done for the day, will refactor code tomorrow

--- controllers/index.js
+++ controllers/index.js
@@ -20,4 +20,49 @@ const getAllStudents = async (req, res) => {
     res.status(500).json({ message: "Error connecting to db", error });
   }
 };
+
+const getSingleStudent = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("students")
+      .findOne({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+};
+
+const createStudent = async (req, res) => {
+  try {
+    const student = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      age: req.body.age,
+      currentCollege: req.body.currentCollege,
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("students")
+      .insertOne(student);
+    if (response.acknowledged) {
+      res.status(200).json({ message: "Student created successfully" });
+    } else {
+      res
+        .status(500)
+        .json(
+          response.error || "Error connecting to db while creating student"
+        );
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 module.exports = { awesomeFunction, tooeleTech, getAllStudents };
